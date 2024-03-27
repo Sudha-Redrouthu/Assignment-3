f0588488fabadff2926131674205212e7e4b2911
Sudha Rani Redroutu <Sredroutu2607@conestogac.on.ca>
2024-03-27 13:58:05 -0400

    Added Rental Agency Class

---
+++ Assignment- 3/RentalAgency.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System;
+
+public class RentalAgency
+{
+    public List<Vehicle> Fleet = new List<Vehicle>();
+    public decimal TotalRevenue { get; private set; }
+
+    public void AddVehicle(Vehicle vehicle)
+    {
+        Fleet.Add(vehicle);
+    }
+
+    public bool RemoveVehicle(Vehicle vehicle)
+    {
+        return Fleet.Remove(vehicle);
+    }
+
+    public void RentVehicle(Vehicle vehicle, int days)
+    {
+        if (Fleet.Contains(vehicle))
+        {
+            TotalRevenue += vehicle.RentalPrice * days;
+            Console.WriteLine($"Vehicle rented for {days} days. Total cost: {vehicle.RentalPrice * days}");
+        }
+        else
+        {
+            Console.WriteLine("Vehicle not available in the fleet.");
+        }
+    }
+}
